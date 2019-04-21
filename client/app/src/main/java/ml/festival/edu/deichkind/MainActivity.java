4b1cb4193227abcda9b060f4afe931e0b5dc1a1a
SniperGER <janikschmidt@icloud.com>
2019-04-21 19:53:54 +0200

    We don't use Kotlin here (also added JSON parsing test)

---
+++ client/app/src/main/java/ml/festival/edu/deichkind/MainActivity.java
@@ -0,0 +1,161 @@
+package ml.festival.edu.deichkind;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.v4.view.GravityCompat;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.MenuItem;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+import android.widget.Toast;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    ArrayList<PackageObject> packageData;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+        final Context ctx = this;
+        final RequestQueue queue = Volley.newRequestQueue(ctx);
+        final ListView listView = findViewById(R.id.list_view);
+
+        StringRequest request = new StringRequest(Request.Method.GET, "https://repo.festival.ml/api/packages", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                packageData = new Gson().fromJson(response, new TypeToken<ArrayList<PackageObject>>() {
+                }.getType());
+                System.out.println(packageData.get(0).identifier);
+
+                List<Map<String, String>> listArray = new ArrayList<>();
+                for (int i = 0; i < packageData.toArray().length; i++) {
+                    Map<String, String> listItem = new HashMap<>();
+                    listItem.put("titleKey", packageData.get(i).name);
+                    listItem.put("subtitleKey", packageData.get(i).shortDescription);
+                    listArray.add(listItem);
+                }
+
+                SimpleAdapter adapter = new SimpleAdapter(ctx, listArray,
+                        android.R.layout.simple_list_item_2,
+                        new String[]{"titleKey", "subtitleKey"},
+                        new int[]{android.R.id.text1, android.R.id.text2});
+                listView.setAdapter(adapter);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(ctx, "Fuck Kotlin!", Toast.LENGTH_LONG).show();
+            }
+        });
+        queue.add(request);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+            Intent intent = new Intent(this, TestActivity.class);
+            startActivity(intent);
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_tools) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+}
+
+class PackageObject {
+    public String id;
+    public String name;
+    public String identifier;
+    public String shortDescription;
+    public String detailedDescription;
+}
