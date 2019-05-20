7db2d11d44d87fd06b3693f97e8385070f820934
Väinämö Łūmikērø <vainamo@git.festival.ml>
2019-05-20 14:05:23 +0200

    Auto stash before merge of "develop" and "origin/develop"

--- client/app/src/main/java/edu/festival/deichkind/loaders/DykeListAsyncTaskLoader.kt
+++ client/app/src/main/java/edu/festival/deichkind/loaders/DykeListAsyncTaskLoader.kt
@@ -5,6 +5,7 @@ import android.support.v4.content.AsyncTaskLoader
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import edu.festival.deichkind.models.Dyke
+import java.io.File
 import java.net.URL
 
 class DykeListAsyncTaskLoader(context: Context) : AsyncTaskLoader<Array<Dyke>>(context) {
@@ -12,8 +13,20 @@ class DykeListAsyncTaskLoader(context: Context) : AsyncTaskLoader<Array<Dyke>>(c
     private var data: Array<Dyke> = arrayOf()
 
     override fun loadInBackground(): Array<Dyke>? {
-        val jsonResponse = URL("https://edu.festival.ml/deichkind/api/dykes").readText()
-        data = Gson().fromJson(jsonResponse, object : TypeToken<Array<Dyke>>() {}.type)
+        val jsonFile = File(context.filesDir, "dykes.json")
+        var jsonText = ""
+
+        jsonText = if (!jsonFile.exists()) {
+            URL("https://edu.festival.ml/deichkind/api/dykes").readText()
+        } else {
+            jsonFile.readText()
+        }
+
+        if (!jsonFile.exists()) {
+            jsonFile.writeText(jsonText)
+        }
+
+        data = Gson().fromJson(jsonText, object : TypeToken<Array<Dyke>>() {}.type)
 
         return data
     }
