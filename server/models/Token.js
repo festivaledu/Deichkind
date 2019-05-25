65bbf865ef73d72435a6241a4446fb1192f8d43e
SniperGER <janikschmidt@icloud.com>
2019-05-25 10:39:09 +0200

    Added support for JWT refresh tokens

---
+++ server/models/Token.js
@@ -0,0 +1,40 @@
+'use strict';
+
+const Sequelize = require("Sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+	const Token = sequelize.define('Token', {
+		id: {
+			type: DataTypes.TEXT,
+			primaryKey: true,
+			unique: true,
+			allowNull: false,
+		},
+		content: {
+			type: DataTypes.JSON,
+			allowNull: false
+		},
+		expires: {
+			type: DataTypes.DATE,
+			allowNull: false
+		}
+	}, {
+		hooks: {
+			beforeCreate(tokenObj, options) {
+				Token.findAll({
+					where: {
+						expires: {
+							[Sequelize.Op.lt]: new Date()
+						}
+					}
+				}).then(tokenList => {
+					tokenList.forEach(tokenObj => {
+						tokenObj.destroy();
+					});
+				});
+			}
+		}
+	});
+	
+	return Token;
+};
